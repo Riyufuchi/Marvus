5349af723ae3e204ff3cefab6824d52c755a7b4f
riyufuchi <krevelixtv@gmail.com>
2024-06-17 23:54:04 +0200

    Update 2.07
    Added:
    * More JUnit tests
    
    Improved:
    * Code readability
    
    Removed:
    * Dead code

--- src/main/java/riyufuchi/marvus/app/windows/TransactionIO.java
+++ src/main/java/riyufuchi/marvus/app/windows/TransactionIO.java
@@ -1,19 +1,17 @@
 package riyufuchi.marvus.app.windows;
 
 import java.io.IOException;
-import java.util.LinkedList;
 
-import riyufuchi.marvus.marvusLib.data.Transaction;
-import riyufuchi.marvus.marvusLib.database.MarvusDatabase;
 import riyufuchi.marvus.marvusLib.interfaces.MarvusDataFrame;
 import riyufuchi.marvus.marvusLib.io.MarvusIO;
+import riyufuchi.marvus.marvusLib.records.FileInput;
 import riyufuchi.sufuLib.gui.SufuFileChooser;
 import riyufuchi.sufuLib.utils.gui.SufuDialogHelper;
 
 /**
  * @author Riyufuchi
- * @version 25.12.2023
  * @since 27.03.2023
+ * @version 17.06.2024
  */
 public class TransactionIO extends SufuFileChooser
 {
@@ -43,18 +41,17 @@ public class TransactionIO extends SufuFileChooser
 	@Override
 	protected void onLoad(String path)
 	{
-		path = addExtension(path);
-		LinkedList<?> list = null;
+		FileInput fi;
 		try
 		{
-			list = MarvusIO.loadData(path);
+			fi = MarvusIO.inputFile(addExtension(path));
 		}
 		catch (ClassNotFoundException | NullPointerException | ClassCastException | IOException e)
 		{
-			SufuDialogHelper.exceptionDialog(budgetDataTable.getSelf(), e);
+			SufuDialogHelper.exceptionDialog(parentFrame, e);
 			return;
 		}
-		setData(path, list);
+		fi.setDataTo(budgetDataTable.getController());
 		budgetDataTable.displayData();
 	}
 	
@@ -63,15 +60,5 @@ public class TransactionIO extends SufuFileChooser
 		if (!path.contains("."))
 			return path += getFileFilter().getDescription();
 		return path;
-	}
-	
-	@SuppressWarnings("unchecked")
-	private void setData(String path, LinkedList<?> list)
-	{
-		switch (MarvusIO.getExtension(path))
-		{
-			case ".dat" -> budgetDataTable.getController().setDatabase((MarvusDatabase)list.getFirst());
-			default -> budgetDataTable.getController().getDatabase().addAll((LinkedList<Transaction>)list);
-		}
 	}
 }
